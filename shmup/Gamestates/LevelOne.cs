8844a4c4b40b9eabd3c45a8efa93a392c65a8cfb
tkirk9155 <32024302+tkirk9155@users.noreply.github.com>
2019-03-01 13:00:44 -0500

    gamestate
    spawning enemies, need to write enemy classes

--- shmup/Gamestates/LevelOne.cs
+++ shmup/Gamestates/LevelOne.cs
@@ -16,14 +16,46 @@ namespace shmup.Gamestates
 {
     public static class LevelOne
     {
-        private static List<Enemy> _enemies = new List<Enemy>()
-        {
-            new Enemy(Textures.enemy01);
-        }
+        private static List<Enemy> _enemies = new List<Enemy>();
+        private static int _framesElapsed = 0;
+        private static int _secondsElapsed = 0;
+
 
         static void Start()
         {
 
         }
+
+        static void Update()
+        {
+            switch (_secondsElapsed)
+            {
+                case 8:
+                    switch (_framesElapsed)
+                    {
+                        // arbitrary numbers
+                        case 0:
+                            _enemies.Add(new Enemy01() { Velocity = 5f, Origin = new Vector2(10f, 10f) });
+                            break;
+                        case 30:
+                            _enemies.Add(new Enemy01() { Velocity = -5f, Origin = new Vector2(-10f, -10f) });
+                            break;
+                        case 60:
+                            _enemies.Add(new Enemy01() { Velocity = 5f, Origin = new Vector2(10f, 10f) });
+                            break;
+                    }
+                    break;
+            }
+
+            _framesElapsed++;
+            if (_framesElapsed == 10)
+            {
+                _framesElapsed = 0;
+                _secondsElapsed++;
+            }
+        }
+
     }
+
 }
+
